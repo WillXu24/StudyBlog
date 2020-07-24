c91ee8ba2da2540a0947962aea1bcb7e8c21bc9a
Will Xu <willxu24@qq.com>
2020-07-24 12:55:33 +0800

    :zap: add fibonnacci

--- go-search/README.md
+++ go-search/README.md
@@ -15,6 +15,12 @@ func SequenceSearch(a []int,b int) int {
 }
 ```
 
+算法分析：简单遍历，线性表就可以
+
+复杂度分析：
+
+- 时间复杂度：**O(n)**
+
 
 
 ### 二分查找
@@ -56,6 +62,12 @@ func bSearch(a[]int,l,h,b int) int {
 }
 ```
 
+算法分析：**有序**查找！注意递归实现的思想。
+
+复杂度分析：
+
+- 时间复杂度：**O(log****2****n)**
+
 
 
 ### 插值查找
@@ -79,3 +91,70 @@ func InsertSearch(a []int, b int) int {
 }
 ```
 
+算法分析：二分查找的改进版，m值的确定变为自适应，可以在一定程度上提高查找效率。
+
+复杂度分析：
+
+- 时间复杂度：**O(log2(log2n))**
+
+
+
+### 斐波那契查找
+
+```go
+func FibonacciSearch(a []int ,b int) int {
+    // 先构造一个斐波那契数列
+	var f []int
+	f=append(f,1,1)
+	for i:=2;i<100;i++{
+		f=append(f,f[i-1]+f[i-2])
+	}
+
+    // 计算临时数列的容量
+	var n int
+	for f[n]-1<len(a){
+		n++
+	}
+
+    // 创建临时数列
+	temp:=make([]int,f[n]-1)
+	for i,v:=range a{
+		temp[i]=v
+	}
+    // 后面的部分用最后一位补齐
+	for i:=len(a);i<len(temp);i++{
+		temp[i]=a[len(a)-1]
+	}
+
+    // 查找部分
+	l,m,h:=0,0,len(a)
+	for l<=h{
+        // 黄金分割点
+		m = l+f[n-1]-1
+		if temp[m] == b {
+            // 如果查找到补齐部分返回数列末尾下标
+			if m<len(a){
+				return m
+			}else {
+				return len(a)-1
+			}
+		}
+		if temp[m] > b {
+			h=m-1
+			n=n-1
+		}
+		if temp[m] < b {
+			l = m + 1
+			n=n-2
+		}
+	}
+
+	return -1
+}
+```
+
+算法分析：二分查找的改进版，利用黄金分割的特点，但是对于数组的长度有要求。
+
+复杂度分析：
+
+- 时间复杂度：**O(log2****n)**
--- go-search/main.go
+++ go-search/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(SequenceSearch([]int{9, 1, 5, 8, 3, 7, 4, 6, 2}, 2))
+	//fmt.Println(SequenceSearch([]int{9, 1, 5, 8, 3, 7, 4, 6, 2}, 2))
+	//
+	//fmt.Println(BinarySearch0([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4))
+	//fmt.Println(BinarySearch1([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 6))
+	//
+	//fmt.Println(InsertSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 5))
 
-	fmt.Println(BinarySearch0([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4))
-	fmt.Println(BinarySearch1([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 6))
-
-	fmt.Println(InsertSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 5))
+	fmt.Println(FibonacciSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 9))
 }
 
 /**********************************************************************************************************************/
@@ -72,5 +74,49 @@ func InsertSearch(a []int, b int) int {
 			l = m + 1
 		}
 	}
+	return -1
+}
+
+/**********************************************************************************************************************/
+func FibonacciSearch(a []int ,b int) int {
+	var f []int
+	f=append(f,1,1)
+	for i:=2;i<100;i++{
+		f=append(f,f[i-1]+f[i-2])
+	}
+
+	var n int
+	for f[n]-1<len(a){
+		n++
+	}
+
+	temp:=make([]int,f[n]-1)
+	for i,v:=range a{
+		temp[i]=v
+	}
+	for i:=len(a);i<len(temp);i++{
+		temp[i]=a[len(a)-1]
+	}
+
+	l,m,h:=0,0,len(a)
+	for l<=h{
+		m = l+f[n-1]-1
+		if temp[m] == b {
+			if m<len(a){
+				return m
+			}else {
+				return len(a)-1
+			}
+		}
+		if temp[m] > b {
+			h=m-1
+			n=n-1
+		}
+		if temp[m] < b {
+			l = m + 1
+			n=n-2
+		}
+	}
+
 	return -1
 }
