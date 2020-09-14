228de02b9a566353c3540c669523b1562559c75c
Will Xu <willxu24@qq.com>
2020-09-14 13:13:07 +0800

    :zap: update

--- go-search/main.go
+++ go-search/main.go
@@ -6,11 +6,11 @@ func main() {
 	//fmt.Println(SequenceSearch([]int{9, 1, 5, 8, 3, 7, 4, 6, 2}, 2))
 	//
 	//fmt.Println(BinarySearch0([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 4))
-	//fmt.Println(BinarySearch1([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 6))
+	fmt.Println(BinarySearch1([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 0))
 	//
-	//fmt.Println(InsertSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 5))
+	fmt.Println(InsertSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 8))
 
-	fmt.Println(FibonacciSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 9))
+	//fmt.Println(FibonacciSearch([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, 9))
 }
 
 /**********************************************************************************************************************/
@@ -46,6 +46,9 @@ func BinarySearch1(a []int, b int) int {
 }
 
 func bSearch(a []int, l, h, b int) int {
+	if l>h{
+		return -1
+	}
 	m := (l + h) / 2
 	if a[m] == b {
 		return m
