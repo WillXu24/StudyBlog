8f180e13cce881e9e2514b5656c2e69a22a356ef
willxu <willxu24@qq.com>
2021-01-08 12:21:21 +0800

    :sparkles: add AES

---
+++ encryption-lab/file-aes/RoyType.h
@@ -0,0 +1,12 @@
+//��ͷ�ļ�û��ʵ����
+//��Ϊ�����涨�����һЩ���ͱ���
+
+#pragma once
+
+#include <iostream>
+
+
+namespace Roy {
+    typedef std::string String;
+    typedef uint8_t state_t[4][4];
+}
---
+++ encryption-lab/file-aes/main.cpp
@@ -0,0 +1,59 @@
+#include <io.h>
+#include "FileAES.h"
+
+#define AES_DEFAULT_KEY "ABCDEFGHIJKLMNOP"
+
+int main(int argc, char *argv[]) {
+    using std::cout;
+    using std::endl;
+    cout << "主函数收到的参数：" << endl;
+    for (int i = 0; i != argc; ++i) {
+        cout << "argv[" << i << "] = " << argv[i] << endl;
+    }
+    Roy::AESParams *ptr = Roy::FileAES::FetchParams(argv, argc);
+    if (!ptr) {
+        cout << "获取参数失败！" << endl;
+        return 1;
+    }
+    Roy::Mode mode = ptr->mode;
+    Roy::String fin = ptr->srcPath;
+    Roy::String fout = ptr->outPath;
+    uint8_t *key = ptr->key;
+    delete (ptr);
+    ptr = NULL;
+    if (fin.empty()) {
+        cout << "输入文件（fin）未指定！" << endl;
+        return 2;
+    }
+    if (fout.empty()) {
+        cout << "输出文件（fout）未指定！" << endl;
+        return 3;
+    }
+    const char *outPath = fout.c_str();
+    if (0 == _access(outPath, 0)) {
+        if (0 != remove(outPath)) {
+            cout << "输出文件存在同名文件！且删除失败！" << endl;
+            return -1;
+        }
+    }
+    if (0 == key) {
+        key = (uint8_t *) AES_DEFAULT_KEY;
+    }
+    int code;
+    switch (mode) {
+        case Roy::Mode::Encrypt:
+            code = Roy::FileAES::EncryptFile(fin.c_str(), fout.c_str(), key);
+            break;
+        case Roy::Mode::Decrypt:
+            code = Roy::FileAES::DecryptFile(fin.c_str(), fout.c_str(), key);
+            break;
+        case Roy::Mode::Unspecified:
+        default:
+            cout << "Mode未指定！" << endl;
+            code = 2;
+    }
+    if (0 == code) {
+        cout << ((mode == Roy::Mode::Encrypt) ? "加密完成！" : (mode == Roy::Mode::Decrypt) ? "解密完成！" : "") << endl;
+    }
+    return code;
+}
